62f0cb93208ae6cb0697c507288b1934c967136c
duanyiting2018 <1036179833@qq.com>
2018-11-14 20:00:58 +0800

    float int

---
+++ 4.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	  system("color f2");
+    printf("     *\n     **");
+	system("pause");
+	return 0;
+}
---
+++ 5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int a,b,c;
+    a=123456789;b=43214321;c=a+b;
+    printf("%d",c);
+	system("pause");
+	return 0;
+}
