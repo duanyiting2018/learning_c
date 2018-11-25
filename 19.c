1de7d2c37b7c029266d04a4207d79c04bb683fa9
duanyiting2018 <1036179833@qq.com>
2018-11-25 14:51:32 +0800

    c learning

---
+++ 19.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int a,b,c;
+    scanf("%d%d",&a,&b);
+    if(a>b)
+    {
+    c=a;
+    }
+    else
+    {
+    c=b;
+    }
+    printf("%d",c);
+	system("pause");
+	return 0;
+}
---
+++ 21.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int i;
+    //for i in range(100);
+    for (i=0;i<100;i++)
+    {
+    printf("wa\n");
+    printf("%d\n",i);
+    }
+	system("pause");
+	return 0;
+}
