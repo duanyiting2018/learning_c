4cb2e1cca3767a92ae9d2f8b4c0094b510642d45
duanyiting2018 <1036179833@qq.com>
2019-08-31 21:19:29 +0800

    75

--- 75.c
+++ 75.c
@@ -1,17 +1,30 @@
 #include<stdio.h>
+struct queue
+{
+	int data[100];
+	int head;
+	int tail;
+};
 int main()
 {
-	int q[102]={0,6,3,1,7,5,8,9,2,4},head,tail;//q[0]不用 
+	struct queue q;
+	int i;
 	//初始化队列 
-	head=1;
-	tail=10;//指向队尾的后一位 
-	while(head<tail)
+	q.head=1;
+	q.tail=1;//指向队尾的后一位 
+	for(i=1;i<=9;i++)
 	{
-		printf("%d ",q[head]);
-		head++;
-		q[tail]=q[head];
-		tail++;
-		head++;
+		//插入九个数 
+		scanf("%d ",&q.data[q.tail]);
+		q.tail++;
+	}
+	while(q.head<q.tail)
+	{
+		printf("%d ",q.data[q.head]);
+		q.head++;
+		q.data[q.tail]=q.data[q.head];
+		q.tail++;
+		q.head++;
 	}
 	system("pause");
 	return 0;
